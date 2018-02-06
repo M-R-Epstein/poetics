9c1dba64ca05b043fe995026a32297f98eaac233
Wallets <max.roy.epstein@gmail.com>
2018-02-06 09:57:25 -0500

    Initial commit.

---
+++ poetics/classes/line.py
@@ -0,0 +1,67 @@
+from poetics.translations import tokenize
+import re
+
+
+class Line:
+    def __init__(self, text, parent=None):
+        self.parent = parent
+        self.plaintext = text
+        self.tokenized_text = None
+        self.word_indexes = ()
+
+        self.initial_word = None
+        self.final_word = None
+
+        self.pos = []
+
+        self.rhyme_candidates = []
+        self.rhyme = None
+
+        self.scansion = []
+
+        # Don't bother tokenizing or finding first/last words if there aren't any letters in the plaintext
+        if re.search('[a-zA-Z]', self.plaintext):
+            self.tokenized_text = tokenize(text)
+            self.initial_word = self.tokenized_text[0]
+            self.final_word = self.tokenized_text[-1]
+
+    def get_rhymes(self):
+        if self.final_word and self.parent:
+            self.rhyme_candidates = self.parent.words[self.final_word].p_rhymes
+        else:
+            return False
+
+    def get_scansion(self):
+        if self.tokenized_text:
+            for word in self.tokenized_text:
+                # If there is only one (unique) stress pattern, then use that.
+                if self.parent.words[word].stresses:
+                    if len(set(self.parent.words[word].stresses)) == 1:
+                        self.scansion.append(self.parent.words[word].stresses[0].replace('2', '0'))
+                    # ToDo: Need to decide what stress pattern to use based on the meter we are generating later
+                    # ToDo: Easiest way might be to just use the first one unless we have a meter calculated
+                    # ToDo: And then to recalculate scansion after all of that business
+                    # ToDo: OR we could have the line store all possible scansions and then resolve them later
+                    # If there are multiple (unique) stress patterns, currently uses the first one.
+                    elif len(set(self.parent.words[word].stresses)) > 1:
+                        self.scansion.append(self.parent.words[word].stresses[0].replace('2', '0'))
+                    # Put in a question mark if we have no stress pattern
+                    else:
+                        self.scansion.append('?')
+                else:
+                    self.scansion.append('?')
+            return self.scansion
+        else:
+            return ''
+
+    # DEPRECATED: Gets the parts of speech for tokenized line. Now done on sentence level.
+    # def get_pos(self):
+    #     if self.pos:
+    #         return self.pos
+    #     else:
+    #         if self.tokenized_text:
+    #             pos_list = nltk.pos_tag(self.tokenized_text)
+    #             for pos in pos_list:
+    #                 self.pos.append(pos[1])
+    #         else:
+    #             return False
---
+++ poetics/classes/word.py
@@ -0,0 +1,61 @@
+import logging
+import nltk
+from poetics.translations import get_phonetic, get_rhymes
+from collections import Counter
+import re
+
+cmudict = nltk.corpus.cmudict.dict()
+
+
+class Word:
+    def __init__(self, word, pronunciations=None, parent=None):
+        self.parent = parent
+        self.plaintext = word
+
+        self.pronunciations = []
+        self.syl_pronunciations = pronunciations
+        self.stresses = []
+
+        self.p_rhymes = []
+        self.word_init_consonants = []
+        self.stressed_vowels = []
+        self.stress_initial_consonants = []
+        self.stress_final_consonants = []
+
+        self.pos = Counter()
+        self.wordnet_pos = Counter()
+        self.synsets = {}
+        # Get a pronunciation if the user hasn't provided one already.
+        if self.syl_pronunciations:
+            logging.info("Pronunciation for \"%s\" provided as \"%s\"", word, self.syl_pronunciations[0])
+            # If the user did provide one, assign it appropriately
+            self.pronunciations.append(re.sub(' - ', ' ', self.syl_pronunciations[0]).split(' '))
+        else:
+            self.pronunciations, self.syl_pronunciations = get_phonetic(word)
+
+        if self.pronunciations:
+            for index, pronunciation in enumerate(self.pronunciations):
+                # Joins all of the sounds together into a single string per pronunciation
+                joined = ''.join(sound for pronunciation in self.pronunciations[index] for sound in pronunciation)
+                # Filters to only keep digits
+                stress = ''.join(filter(lambda x: x.isdigit(), joined))
+                if len(stress) == 1:
+                    self.stresses.append('3')
+                else:
+                    self.stresses.append(stress)
+                # Sets the digit only versions as our stresses
+
+                # Gets rhymes
+                self.p_rhymes, self.word_init_consonants, self.stressed_vowels, self.stress_initial_consonants, \
+                    self.stress_final_consonants = get_rhymes(self.pronunciations, self.syl_pronunciations)
+
+    # Places (lists of) synsets in self.synsets under their pos key
+    def get_synsets(self):
+        from nltk.corpus import wordnet
+        for pos in self.wordnet_pos:
+            returned = wordnet.synsets(self.plaintext, pos=pos)
+            # Note: Should probably try specifying no part of speech if we get no return. Depends on quality of tagger.
+            # TODO: Need to check out Morphy for handling word forms not in wordnet
+            logging.debug('Search for %s as %s returned:\n %s', self.plaintext, pos, returned)
+            if returned:
+                self.synsets[pos] = returned
