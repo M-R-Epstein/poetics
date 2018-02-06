fdbd0dc03a0c5ac0670408e841599520afcad3ff
Wallets <max.roy.epstein@gmail.com>
2018-02-06 09:30:32 -0500

    Initial commit.

---
+++ poetics/__init__.py
@@ -0,0 +1,1 @@
+from poetics.utilities import create_poem, process_poems
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='poetics',
+      version='0.1',
+      description='Tools for poetry analysis',
+      url='https://github.com/M-R-Epstein/poetics',
+      author='Max Epstein',
+      author_email='x@x.com',
+      license='MIT',
+      packages=['poetics'],
+      zip_safe=False)
