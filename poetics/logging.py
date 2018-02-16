8dd05bf177fa0d585db97fec6157d058d5860024
Wallets <max.roy.epstein@gmail.com>
2018-02-16 15:09:12 -0500

    Changed scansion to use tags_with_text.

--- poetics/logging.py
+++ poetics/logging.py
@@ -1,26 +1,17 @@
 import logging
 
 
-# Logs a scansion in a readable form.
-def print_scansion(scansion, prefix=''):
-    if prefix:
-        prefix = prefix + ' '
-    # Business with index_mod is to handle line numbers while accounting for blanks.
-    index_mod = 1
-    logging.info("%sScansion:", prefix)
-    # Reformats 3/4 (stressed/unstressed single syllable words).
-    for index, scan in enumerate(scansion):
-        scansion[index] = scansion[index].replace("3", "̲0")
-        scansion[index] = scansion[index].replace("4", "̲1")
-        if scan == '':
-            logging.info('')
-            index_mod -= 1
-        else:
-            logging.info("%s (%s)", scansion[index], index + index_mod)
+def convert_scansion(scansion):
+    converted_scan = []
+    for scan in scansion:
+        converted = scan.replace('0', 'u')
+        converted = converted.replace('1', '/')
+        converted_scan.append(converted)
+    return converted_scan
 
 
 # Takes a list of tags and a list of tokenized words and attempts to align them in a readable form in the log.
-def tags_under_text(tokenized_text, tags):
+def tags_with_text(tokenized_text, tags, line_num=None, above=False):
     line_out = ''
     offset = 0
     # Use a bunch of nonsense to center the tags under the words
@@ -39,9 +30,15 @@ def tags_under_text(tokenized_text, tags):
             line_out += tags[index] + ' '
             # Offset is just keeping track of how far we are pushed to the right by tag length>word length
             offset += dif
-    # Output each line with the POS tags under it
-    logging.info(' '.join(tokenized_text))
-    logging.info(line_out)
+    # Output each line with the tags over/under
+    if above is True:
+        logging.info(line_out)
+    if line_num:
+        logging.info("%s (%s)", ' '.join(tokenized_text), line_num)
+    else:
+        logging.info(' '.join(tokenized_text))
+    if above is False:
+        logging.info(line_out)
 
 
 # Takes a sound set and prints it.
