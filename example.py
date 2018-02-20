215f74927054120d357855bb9a2c981f0c3a7e09
Wallets <max.roy.epstein@gmail.com>
2018-02-20 09:14:23 -0500

    Updated functions.

--- example.py
+++ example.py
@@ -7,13 +7,18 @@ import poetics
 coloredlogs.install(level='INFO', fmt='%(asctime)s: %(message)s', datefmt='%H:%M:%S', stream=sys.__stdout__)
 
 # Process all poems in the /poems folder. Optional argument to specify a directory, defaults to /poems otherwise.
-# poetics.process_poems()
+poetics.process_poems()
 
 # Single Poem operations.
 # Optional arguments for create_poem() are title, author, and directory
 poem = poetics.poetics.create_poem('when my light is spent-john milton.txt')
 poem.get_rhymes()
+poem.get_sonic_features()
 poem.get_pos()
 poem.get_scansion()
+poem.get_meter_v_scan()
 poem.get_meter()
+poem.get_form()
 poem.record()
+
+
