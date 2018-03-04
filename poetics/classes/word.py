bcdfb0e7cfa5858c540a009cc4701e0cec3725b4
Wallets <max.roy.epstein@gmail.com>
2018-03-04 08:08:01 -0500

    Printing and immutability.

--- poetics/classes/word.py
+++ poetics/classes/word.py
@@ -1,58 +1,62 @@
 import logging
-import re
-from collections import Counter
 
-from nltk.corpus import wordnet
-
-from poetics.lookups import get_phonetic, get_rhymes
+from poetics.lookups import get_pronunciations
+from poetics.classes.pronunciation import Pronunciation
 
 
 class Word:
-    def __init__(self, word, pronunciations=None, parent=None):
+    def __init__(self, word, user_pronunciation=None, parent=None):
         self.parent = parent
-        self.plaintext = word
+        self.token = word
 
-        self.pronunciations = []
-        self.syl_pronunciations = pronunciations
-        self.stresses = []
+        self.pronunciations = None
 
-        self.p_rhymes = []
-        self.word_init_consonants = []
-        self.stressed_vowels = []
-        self.stress_initial_consonants = []
-        self.stress_final_consonants = []
-        self.stress_bracket_consonants = []
-
-        self.pos = Counter()
-        self.wordnet_pos = Counter()
-        self.synsets = {}
-
-        # Get a pronunciation if the user hasn't provided one already.
-        if self.syl_pronunciations:
-            logging.info("Pronunciation for \"%s\" provided as \"%s\"", word, self.syl_pronunciations[0])
-            # If the user did provide one, assign it appropriately
-            self.pronunciations.append(re.sub(' - ', ' ', self.syl_pronunciations[0]).split(' '))
+        # Deal with user provided pronunciations.
+        if user_pronunciation:
+            vowels = ['AA', 'AE', 'AH', 'AO', 'AW', 'AX', 'AY', 'EH', 'ER', 'EY', 'IH', 'IX', 'IY', 'OW', 'OY', 'UH',
+                      'UW']
+            logging.info("Pronunciation for \"%s\" provided as \"%s\"", word, user_pronunciation)
+            out_pronunciation = []
+            f_pronunciations = []
+            syllables = user_pronunciation.split('-')
+            for syllable in syllables:
+                syllable_out = ["", "", "", ""]
+                strip = syllable.strip()
+                if "1" in strip:
+                    syllable_out[0] = 1
+                    strip = strip.replace("1", "")
+                else:
+                    syllable_out[0] = 0
+                    strip = strip.replace("2", "")
+                split = strip.split()
+                onset = []
+                coda = []
+                try:
+                    nucleus = next(phoneme for phoneme in split if phoneme in vowels)
+                    syllable_out[2] = nucleus
+                    nucleus_index = split.index(nucleus)
+                    for phoneme in split[:nucleus_index]:
+                        onset.append(phoneme)
+                    syllable_out[1] = ' '.join(onset)
+                    for phoneme in split[nucleus_index + 1:]:
+                        coda.append(phoneme)
+                    syllable_out[3] = ' '.join(coda)
+                except StopIteration:
+                    for phoneme in split:
+                        onset.append(phoneme)
+                    syllable_out[1] = ' '.join(onset)
+                out_pronunciation.append(syllable_out)
+                f_pronunciations.append(Pronunciation(out_pronunciation, self))
+                self.pronunciations = tuple(f_pronunciations)
         else:
-            self.pronunciations, self.syl_pronunciations = get_phonetic(word)
+            f_pronunciations = []
+            pronunciations = get_pronunciations(word)
+            for pronunciation in pronunciations:
+                f_pronunciations.append(Pronunciation(pronunciation, self))
+            self.pronunciations = tuple(f_pronunciations)
 
-        if self.pronunciations:
-            stress_list = []
-            for index, pronunciation in enumerate(self.pronunciations):
-                # Joins all of the sounds together into a single string per pronunciation
-                joined = ''.join(sound for pronunciation in self.pronunciations[index] for sound in pronunciation)
-                # Filters to only keep digits
-                stress = ''.join(filter(lambda x: x.isdigit(), joined)).replace('2', '0')
-                stress_list.append(stress)
-            self.stresses = list(set(stress_list))
-            (self.p_rhymes, self.word_init_consonants, self.stressed_vowels, self.stress_initial_consonants,
-             self.stress_final_consonants, self.stress_bracket_consonants) = get_rhymes(self.pronunciations,
-                                                                                        self.syl_pronunciations)
+    def __str__(self) -> str:
+        return self.token
 
-
-# Places (lists of) synsets in self.synsets under their pos key.
-def get_synsets(self):
-    for pos in self.wordnet_pos:
-        returned = wordnet.synsets(self.plaintext, pos=pos)
-        logging.debug('Search for %s as %s returned:\n %s', self.plaintext, pos, returned)
-        if returned:
-            self.synsets[pos] = returned
+    def __repr__(self) -> str:
+        return '%s (%s)' % (super().__repr__(), self.token)
