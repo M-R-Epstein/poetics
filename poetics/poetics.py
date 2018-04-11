7988c664d0e9092c55469a5b2aa6359dd9c9d592
Wallets <max.roy.epstein@gmail.com>
2018-04-11 18:00:31 -0400

    Fixed process_poems().

--- poetics/poetics.py
+++ poetics/poetics.py
@@ -12,8 +12,8 @@ def create_poem(filename, title=None, author=None, directory=config.poem_directo
 
     if not title:
         # If the file is in a directory name the poem the entire name of the text file (minus .txt).
-        if '/' in filename:
-            title = re.search("(?<=/)[^/]+(?=\.txt)", filename).group(0)
+        if '/' in filename or '\\' in filename:
+            title = re.search(r"(?<=[\\/])[^\\/]+(?=\.txt)", filename).group(0)
             if not title:
                 title = "Unknown poem"
                 logging.warning("Title for \"%s\" set as \"Unknown poem\". Title detection failed.", filename)
@@ -29,8 +29,8 @@ def create_poem(filename, title=None, author=None, directory=config.poem_directo
     if not author:
         # If the file is in a directory, use the name of the highest level subdirectory of the poems directory as the
         # author name.
-        if '/' in filename:
-            author = re.search("[^/]+", filename).group(0)
+        if '/' in filename or '\\' in filename:
+            author = re.search(r"[^\\/]+", filename).group(0)
             if not author:
                 author = "Unknown Poet"
                 logging.warning("Author for \"%s\" set as \"Unknown poet\". Author detection failed.", filename)
@@ -57,37 +57,14 @@ def process_poems(directory=config.poem_directory, outputfile=config.output_file
         return None
 
     for dirpath, dirnames, filenames in os.walk(directory):
-        for file in filenames:
-
-            # Extract the filename if the file is in a subdirectory of poem_directory.
-            if '/' in file:
-                title = re.search("(?<=/)[^/]+(?=\.txt)", file).group(0)
-            # Otherwise, assume that the file is named <title>-<author>.txt.
+        relative_path = os.path.relpath(dirpath, directory)
+        for filename in filenames:
+            # Handles root directory
+            if relative_path == ".":
+                file = filename
             else:
-                title = re.search(".+(?=-)", file).group(0)
-
-            if not title:
-                logging.warning("File \"%s\" skipped as no title was detected.", file)
-                continue
-
-            # Take the author name from the top level subdirectory of poem_directory.
-            author_search = re.search("[^/]+", file)
-            if author_search:
-                author = author_search.group(0)
-            else:
-                author = "Unknown"
-
-                logging.warning("Author for \"%s\" set as \"Unknown\" as no name was found. Author names are inherited "
-                                "from the top-level subdirectory of the poem directory in which a poem file is stored "
-                                "by default. Poems directly inside of the poems directly are assumed to have their "
-                                "filenames formatted as \"title-author.txt\".", file)
-
-            # Read in data
-            with open(dirpath + "/" + file, encoding="utf-8") as data:
-                read_data = data.readlines()
-
-            # Create poem, do stuff, record
-            poem = Poem(read_data, title, author)
+                file = os.path.join(relative_path, filename)
+            poem = create_poem(file)
             poem.get_rhymes()
             poem.get_sonic_features()
             poem.get_pos()
